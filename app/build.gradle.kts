985964e8f4172dc25675dc39c3aa1a7e7061c245
Shuvagin <e.shuvagin@tinkoff.ru>
2024-04-30 11:15:03 +0300

    basics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 android {
     namespace = "com.example.camerademo"
     compileSdk = 34
+    buildFeatures.viewBinding = true
 
     defaultConfig {
         applicationId = "com.example.camerademo"
